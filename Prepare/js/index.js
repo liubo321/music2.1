b90c50b996ee8cbda096663a86f9e05bad4de415
Liubo <1003670604@qq.com>
2016-02-18 16:13:32 +0800

    Liu

---
+++ Prepare/js/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by MrLiu on 2016/2/18.
+ */
+
+$(function(){
+    $('#dowebok').fullpage({
+        sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90', '#f09'],
+        anchors: ['page1', 'page2', 'page3', 'page4','page5'],
+        menu: '#menu'
+    });
+});
